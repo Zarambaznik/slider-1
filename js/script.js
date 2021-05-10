a80f8c8e48a7fc2fff90d992d47a5a6e40063a1d
zarambaznik <dimulya.kuznetsov.99@mail.ru>
2021-05-11 00:45:57 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    $('.slider').slick();
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 2,
+        speed: 500,
+        infinite: true,
+        // Остановился на 28:28
+    });
 });
